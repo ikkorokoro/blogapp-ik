3f2baac660865cdee439bccadf5dc596110c3d7f
今吉恒介 <>
2020-10-05 12:38:05 +0900

    createアクション　追加

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module BlogAppIk
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = :ja # 日本語設定
   end
 end
