5ca7476716e58e64c25ddafe5cd4da66bc94cdc1
今吉恒介 <>
2020-10-10 16:27:53 +0900

    herokuの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,18 +2,20 @@
 #
 # Table name: articles
 #
-#  id         :integer          not null, primary key
+#  id         :bigint           not null, primary key
 #  content    :text             not null
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  user_id    :integer          not null
+#  user_id    :bigint           not null
 #
 # Indexes
 #
 #  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
+  has_one_attached :eyecatch
+  
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
   validates :title, format: { with: /\A(?!\@)/ }#@から始まっていないかチェック
@@ -26,6 +28,7 @@ class Article < ApplicationRecord
 
 
   belongs_to :user
+  has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   
   def display_created_at
@@ -36,7 +39,9 @@ class Article < ApplicationRecord
     user.display_name
   end
 
-
+  def like_count
+    likes.count
+  end
 
 private
 #独自のvalidatesを追加する
